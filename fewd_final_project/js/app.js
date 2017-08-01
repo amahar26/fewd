cf4b214d7b79de69bf28e119f4ca7f3bd35baf7c
amahar26 <amando262000@yahoo.com>
2017-07-31 22:16:45 -0400

    pushaT

--- fewd_final_project/js/app.js
+++ fewd_final_project/js/app.js
@@ -36,13 +36,20 @@ window.sr = ScrollReveal();
 sr.reveal('nav .sec1', { duration: 1000, easing: 'cubic-bezier(0.6, 0.2, 0.1, 1)'});
 sr.reveal('nav .sec2', { duration: 1000, easing: 'cubic-bezier(0.6, 0.2, 0.1, 1)', delay: 500});
 sr.reveal('nav .sec3', { duration: 1000, easing: 'cubic-bezier(0.6, 0.2, 0.1, 1)', delay: 1000 });
+
+
+$(".sec1").click(function(){
+
 sr.reveal('.reveal-test', { axis: 'x',
   origin: 'right',
   distance: '500px',
   easing   : 'ease-in-out',
   scale: '1',
   duration: '2000',
-  delay: '-300'});
+  delay: '-300'})
+
+
+});
 
 
 //$("#trigger").click(function(){});
