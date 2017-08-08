d1549f732172d563f2e3087eca4f63bad9500511
Amanda <amando262000@yahoo.com>
2017-08-08 12:21:55 -0400

    Update app.js

--- fewd_final_project/js/app.js
+++ fewd_final_project/js/app.js
@@ -16,8 +16,8 @@ if(b.numberStep){var c=this.each(function(){this._animateNumberSetter=b.numberSt
 // }, 1);
 
 $(".work").hide();
-$(".contact").hide();
-$(".social").hide();
+//$(".contact").hide();
+//$(".social").hide();
 
 
 $('.work').slideReveal({
